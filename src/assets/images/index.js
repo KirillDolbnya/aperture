f1dd4ccadfda9befb24fe6132887fa2e7f0d8385
KirillDolbnya <kirilldolbnya6@gmail.com>
2024-12-10 00:25:11 +0300

    add image

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,9 +1,11 @@
 import logo from './Logo.svg';
 import hero from './hero.png';
 import productItem from './productItem.png';
+import button from './button.svg';
 
 export const images = {
     logo: logo,
     hero: hero,
     productItem: productItem,
+    button: button,
 };

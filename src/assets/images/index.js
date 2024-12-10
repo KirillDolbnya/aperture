f1c01c64391c6d8eb97fdbe6a4d179d18e14378c
Kirill Dolbnya <kirilldolbnya6@gmail.com>
2024-12-10 12:06:47 +0300

    add images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,10 +2,18 @@ import logo from './Logo.svg';
 import hero from './hero.png';
 import productItem from './productItem.png';
 import button from './button.svg';
+import time from './time.svg';
+import social from './social.svg';
+import iso from './iso.svg';
+import location from './location.svg';
 
 export const images = {
     logo: logo,
     hero: hero,
     productItem: productItem,
     button: button,
+    time: time,
+    social: social,
+    iso: iso,
+    location: location,
 };

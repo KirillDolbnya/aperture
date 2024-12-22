1951420d6a970399d8d4ea078c8b6c8103adeb02
KirillDolbnya <kirilldolbnya6@gmail.com>
2024-12-23 00:21:27 +0300

    add images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -6,6 +6,14 @@ import time from './time.svg';
 import social from './social.svg';
 import iso from './iso.svg';
 import location from './location.svg';
+import app_location from './app-location.png';
+import nepal from './nepal.png';
+import costarica from './costarica.png';
+import logoClientOne from './logo-1.png';
+import logoClientTwo from './logo-2.png';
+import logoClientThree from './logo-3.png';
+import logoClientFour from './logo-4.png';
+import logoClientFive from './logo-5.png';
 
 export const images = {
     logo: logo,
@@ -16,4 +24,12 @@ export const images = {
     social: social,
     iso: iso,
     location: location,
+    app_location: app_location,
+    nepal: nepal,
+    costarica: costarica,
+    logoClientOne: logoClientOne,
+    logoClientTwo: logoClientTwo,
+    logoClientThree: logoClientThree,
+    logoClientFour: logoClientFour,
+    logoClientFive: logoClientFive,
 };

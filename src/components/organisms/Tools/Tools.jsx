9068e0a50ae28370c6240da2a4c896f095ced11e
Kirill Dolbnya <kirilldolbnya6@gmail.com>
2024-12-26 18:33:04 +0300

    add new component

--- src/components/organisms/Tools/Tools.jsx
+++ src/components/organisms/Tools/Tools.jsx
@@ -2,18 +2,43 @@ import "./style.scss";
 import { ButtonPicture } from "@/components/molecules/ButtonPicture/ButtonPicture";
 import { Tag } from "@/components/molecules/Tag/Tag";
 
-export function Tools({title,text,image}){
+export function Tools({slag,title,text,image}){
+    const tagsData = [
+        {
+            title: '0,8"',
+            type: 'time',
+        },
+        {
+            title: 'f/5,6',
+            type: 'social',
+        },
+        {
+            title: '100',
+            type: 'iso',
+        },
+        {
+            title: 'Sweden',
+            type: 'location',
+        },
+    ]
+
     return(
       <div className="app-tools">
           <div className="app-tools__left">
-
+              <p className="app-tools__slag">{slag}</p>
+              <p className="app-tools__title">{title}</p>
+              <p className="app-tools__desc">{text}</p>
+              <ButtonPicture disabled={false} onClick={(e) => e.preventDefault()}>
+                  Check it out
+              </ButtonPicture>
           </div>
-          <div className="app-tools__right">
+          <div className="app-tools__right" style={{ backgroundImage: `url(${image})` }}>
               <div className="app-tools__tags">
-                  <Tag/>
-                  <Tag/>
-                  <Tag/>
-                  <Tag/>
+                  {
+                      tagsData.map(tag =>
+                          <Tag title={tag.title} type={tag.type} />
+                      )
+                  }
               </div>
           </div>
       </div>
--- src/components/templates/HomeTemplate.jsx
+++ src/components/templates/HomeTemplate.jsx
@@ -4,6 +4,7 @@ import { ProductItems } from "@/components/organisms/ProductItems/ProductItems";
 import { Location } from "@/components/organisms/Location/Location";
 import { Clients } from "@/components/organisms/Clients/Clients";
 import { Connection } from "@/components/organisms/Connection/Connection";
+import { Tools } from "@/components/organisms/Tools/Tools";
 import { images } from "@/assets/images/index.js";
 
 export function HomeTemplate() {
@@ -70,6 +71,7 @@ export function HomeTemplate() {
         <Hero />
         <ProductItems />
         <Location title="Sunset at Mount Fuji" subtitle="Duis tristique sed lorem a vestibulum. Cras commodo consequat orci, in convallis risus egestas non." image={images.app_location} tagsData={tagsJapan} />
+        <Tools slag="The Gear cage" title="The tools that we use." text="The say that 'no place is boring if youve had a good night s sleep and have a pocket full of unexposed film''. While we don't shoot (a lot) of film these days — these are the tools that we actually use everyday to capture the amazing things around us." image={images.tools} />
         <Location title="Monstera Leafs" subtitle="Vestibulum sit amet urna turpis. Mauris euismod elit et nisi ultrices, ut faucibus orci tincidunt." image={images.costarica} tagsData={tagsCostaRica} />
         <Clients title="Past clients" subtitle="Trusted by your favourite companies" />
         <Location title="Star fall in the Himalayas" subtitle="Nulla rhoncus feugiat eros quis consectetur. Morbi neque ex, condimentum dapibus congue et, vulputate ut ligula." image={images.nepal} tagsData={tagsNepal} />

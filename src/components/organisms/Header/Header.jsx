0c087d0927acfa929f868edd50ddc434ab47f04f
KirillDolbnya <kirilldolbnya6@gmail.com>
2025-01-03 16:34:28 +0300

    fix component Header

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -3,12 +3,19 @@ import { Navbar } from "@/components/molecules/Navbar/Navbar";
 import "./style.scss"
 
 export function Header() {
+    const links = [
+        { name: 'Business areas' },
+        { name: 'Featured images' },
+        { name: 'Gear cage' },
+        { name: 'Contact' },
+    ];
+
     return (
         <header className="app-header">
             <div className="container">
                 <div className="app-header__container">
                     <Logo />
-                    <Navbar />
+                    <Navbar className={'navbar'} links={links} disabled={false}  />
                 </div>
             </div>
         </header>
